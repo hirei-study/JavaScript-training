ae748791f62c9e295acb5544752865f5aaf45acc
hirei <93576357+hirei-study@users.noreply.github.com>
2023-06-22 06:50:28 +0000

    Update 4 files

--- src/index.js
+++ src/index.js
@@ -1,10 +1,58 @@
 import "./styles.css";
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+// // const let などの変数宣言
+// var value1 = 1;
+// console.log("value1: " + value1);
+
+// // var は上書きが可能
+// value1 = "aaa";
+// console.log("value1: " + value1);
+
+// // var 変数は再宣言可能
+// var value1 = "value1を再宣言";
+// console.log("value1: " + value1);
+
+// let value2 = 2;
+// console.log("value2: " + value2);
+
+// // let は上書きが可能
+// value2 = "bbb";
+// console.log("value2: " + value2);
+
+// // let は再宣言はできない
+// let value2 = "value2を再宣言";
+// console.log("value2: " + value2);
+
+// const value3 = 3;
+// console.log("value3: " + value3);
+
+// // const は上書き不可能
+// value3 = "ccc";
+// console.log("value3: " + value3);
+
+// // const は再宣言不可能
+// const value3 = "value3を再宣言";
+// console.log("value3: " * value3);
+
+// const value4 = {
+//   name: "hirei",
+//   age: 27,
+// };
+
+// console.log("value4: " + value4);
+
+// value4.name = "hirei-study";
+// value4.address = "福岡";
+// console.log("value4 オブジェクトを変更: " + value4);
+
+// const value5 = ["dog", "cat"];
+// value5[0] = "bird";
+// value5.push("monkey");
+// console.log(value5);
+// console.log("value5: " + value5);
+
+// テンプレート文字列
+const name = "hirei";
+const age = 27;
+
+console.log(`私の名前は${name}です。年齢は${age}歳です。`);
