9b248828e0b2e84bdea0ad618598cae8f25c0d08
hirei <93576357+hirei-study@users.noreply.github.com>
2023-06-22 07:19:54 +0000

    Update 3 files

--- src/index.js
+++ src/index.js
@@ -91,10 +91,45 @@ import "./styles.css";
 // const message2 = `名前は${name}です。年齢は${age}歳です。`;
 // console.log(`自己紹介: ${message2}`);
 
-const myProfile2 = ["hirei", 27];
-const message3 = `名前は${myProfile2[0]}です。年齢は${myProfile2[1]}歳です。`;
-console.log(`自己紹介: ${message3}`);
+// const myProfile2 = ["hirei", 27];
+// const message3 = `名前は${myProfile2[0]}です。年齢は${myProfile2[1]}歳です。`;
+// console.log(`自己紹介: ${message3}`);
 
-const [name, age] = myProfile2;
-const message4 = `名前は${name}です。年齢は${age}歳です。`;
-console.log(`自己紹介: ${message4}`);
+// const [name, age] = myProfile2;
+// const message4 = `名前は${name}です。年齢は${age}歳です。`;
+// console.log(`自己紹介: ${message4}`);
+
+// デフォルト値、引数など
+// const sayHello = (name = "A") => {
+//   console.log(`こんにちは。${name}さん。`);
+// };
+// sayHello("hirei");
+
+// スプレッド構文
+// 配列の展開
+// const arr1 = [1, 2];
+// console.log(arr1);
+// console.log(...arr1);
+
+// const summaryFunc = (num1, num2) => {
+//   return num1 + num2;
+// };
+// console.log(`合計: ${summaryFunc(arr1[0], arr1[1])}`);
+// console.log(`合計: ${summaryFunc(...arr1)}`);
+
+// まとめる
+// const arr2 = [1, 2, 3, 4, 5];
+// const [num1, num2, ...arr3] = arr2;
+// console.log(`num1: ${num1}`);
+// console.log(`num2: ${num2}`);
+// console.log(`arr3: ${arr3}`);
+
+// 配列のコピーや結合
+// const arr4 = [10, 20];
+// const arr5 = [30, 40];
+
+// const arr6 = [...arr4];
+// console.log(`arr6: ${arr6}`);
+
+// const arr7 = [...arr4, ...arr5];
+// console.log(`arr7: ${arr7}`);
